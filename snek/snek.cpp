418cc0141c78c1c3dcb310705f4ce6fcc436de54
yany2 <46777856+yany2@users.noreply.github.com>
2023-01-30 14:40:38 +0100

    build and link SDL2 with CMake

--- snek/snek.cpp
+++ snek/snek.cpp
@@ -1,12 +1,12 @@
-﻿// snek.cpp : Defines the entry point for the application.
-//
+﻿#include <iostream>
 
-#include "snek.h"
+#include <SDL.h>
 
 using namespace std;
 
-int main()
-{
+int main(int argc, char** argv) {
+	SDL_Init(SDL_INIT_VIDEO);
 	cout << "Hello CMake." << endl;
 	return 0;
+	SDL_Quit();
 }
