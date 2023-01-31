4371ca62cb80667ded876c6c9d23c432d248a1cf
yany2 <46777856+yany2@users.noreply.github.com>
2023-01-31 18:20:32 +0100

    rename variables

--- snek/snek.cpp
+++ snek/snek.cpp
@@ -32,13 +32,13 @@ struct Tile{
 
 SDL_Texture** textures;
 
-Snek* front;
 SDL_Window* window;
 SDL_Renderer* renderer;
 
 Tile** world;
+Snek* snekFront;
+Direction snekDirection = UP;
 
-Direction dir = UP;
 std::queue<Direction> inputQueue;
 
 const int FIELD = 32;
@@ -69,33 +69,33 @@ void init() {
 			world[i][j].out = NONE;
 		}
 	}
-	front = new Snek;
-	front->x = WIDTH / 2;
-	front->y = HEIGHT / 2;
-	front->next = new Snek;
-	front->next->x = front->x - 1;
-	front->next->y = front->y;
-	front->next->next = new Snek;
-	front->next->next->x = front->x - 2;
-	front->next->next->y = front->y;
-	front->next->next->next = nullptr;
+	snekFront = new Snek;
+	snekFront->x = WIDTH / 2;
+	snekFront->y = HEIGHT / 2;
+	snekFront->next = new Snek;
+	snekFront->next->x = snekFront->x - 1;
+	snekFront->next->y = snekFront->y;
+	snekFront->next->next = new Snek;
+	snekFront->next->next->x = snekFront->x - 2;
+	snekFront->next->next->y = snekFront->y;
+	snekFront->next->next->next = nullptr;
 
-	world[front->x][front->y].type = SNEK;
-	world[front->next->x][front->next->y].type = SNEK;
-	world[front->next->next->x][front->next->next->y].type = SNEK;
+	world[snekFront->x][snekFront->y].type = SNEK;
+	world[snekFront->next->x][snekFront->next->y].type = SNEK;
+	world[snekFront->next->next->x][snekFront->next->next->y].type = SNEK;
 
-	world[front->x][front->y].in = RIGHT;
-	world[front->next->x][front->next->y].in = RIGHT;
-	world[front->next->next->x][front->next->next->y].out = RIGHT;
-	world[front->next->x][front->next->y].out = RIGHT;
+	world[snekFront->x][snekFront->y].in = RIGHT;
+	world[snekFront->next->x][snekFront->next->y].in = RIGHT;
+	world[snekFront->next->next->x][snekFront->next->next->y].out = RIGHT;
+	world[snekFront->next->x][snekFront->next->y].out = RIGHT;
 
-	int nx, ny;
+	int newFoodX, newFoodY;
 	for (int i = 0; i < FOOD_COUNT; i++) {
 		do {
-			nx = rand() % WIDTH;
-			ny = rand() % HEIGHT;
-		} while (world[nx][ny].type != EMPTY);
-		world[nx][ny].type = FOOD;
+			newFoodX = rand() % WIDTH;
+			newFoodY = rand() % HEIGHT;
+		} while (world[newFoodX][newFoodY].type != EMPTY);
+		world[newFoodX][newFoodY].type = FOOD;
 	}
 
 	srand(time(0));
@@ -123,14 +123,14 @@ int main(int argc, char** argv) {
 	bool running = true;
 	int count = 60;
 	while (running) {
-		SDL_Event e;
-		SDL_PollEvent(&e);
-		switch (e.type) {
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch (event.type) {
 		case SDL_QUIT:
 			running = false;
 			break;
 		case SDL_KEYDOWN:
-			switch (e.key.keysym.scancode) {
+			switch (event.key.keysym.scancode) {
 			case SDL_SCANCODE_UP:
 				inputQueue.push(UP);
 				break;
@@ -149,146 +149,146 @@ int main(int argc, char** argv) {
 
 
 		SDL_RenderClear(renderer);
-		SDL_Rect a = { 0, 0, FIELD, FIELD };
+		SDL_Rect renderTarget = { 0, 0, FIELD, FIELD };
 
 
 		count--;
 		if (!count) {
-			Snek* nexttail = front;
+			Snek* nextTail = snekFront;
 			do {
-				nexttail = nexttail->next;
-			} while (nexttail->next->next);
+				nextTail = nextTail->next;
+			} while (nextTail->next->next);
 			count = 15;
-			int prevtailx = nexttail->next->x;
-			int prevtaily = nexttail->next->y;
-			nexttail->next->next = front;
-			world[nexttail->next->x][nexttail->next->y].type = EMPTY;
-			world[nexttail->next->x][nexttail->next->y].out = NONE;
-			Direction prevdir = world[nexttail->x][nexttail->y].in;
-			world[nexttail->x][nexttail->y].in = NONE;
-			nexttail->next->x = front->x;
-			nexttail->next->y = front->y;
-			front = nexttail->next;
-			nexttail->next = nullptr;
+			int previousTailX = nextTail->next->x;
+			int previousTailY = nextTail->next->y;
+			nextTail->next->next = snekFront;
+			world[nextTail->next->x][nextTail->next->y].type = EMPTY;
+			world[nextTail->next->x][nextTail->next->y].out = NONE;
+			Direction previousDirection = world[nextTail->x][nextTail->y].in;
+			world[nextTail->x][nextTail->y].in = NONE;
+			nextTail->next->x = snekFront->x;
+			nextTail->next->y = snekFront->y;
+			snekFront = nextTail->next;
+			nextTail->next = nullptr;
 
-			while (!inputQueue.empty() && (inputQueue.front() == dir || (inputQueue.front() + 2) % 4 == dir))
+			while (!inputQueue.empty() && (inputQueue.front() == snekDirection || (inputQueue.front() + 2) % 4 == snekDirection))
 				inputQueue.pop();
 			if (!inputQueue.empty()) {
-				dir = inputQueue.front();
+				snekDirection = inputQueue.front();
 				inputQueue.pop();
 			}
 
-			switch (dir) {
+			switch (snekDirection) {
 			case UP:
-				front->y--;
-				if (front->y < 0)
-					front->y = HEIGHT - 1;
+				snekFront->y--;
+				if (snekFront->y < 0)
+					snekFront->y = HEIGHT - 1;
 				break;
 			case DOWN:
-				front->y++;
-				if (front->y >= HEIGHT)
-					front->y = 0;
+				snekFront->y++;
+				if (snekFront->y >= HEIGHT)
+					snekFront->y = 0;
 				break;
 			case LEFT:
-				front->x--;
-				if (front->x < 0)
-					front->x = WIDTH - 1;
+				snekFront->x--;
+				if (snekFront->x < 0)
+					snekFront->x = WIDTH - 1;
 				break;
 			case RIGHT:
-				front->x++;
-				if (front->x >= WIDTH)
-					front->x = 0;
+				snekFront->x++;
+				if (snekFront->x >= WIDTH)
+					snekFront->x = 0;
 				break;
 			}
-			world[front->x][front->y].in = dir;
-			world[front->next->x][front->next->y].out = dir;
+			world[snekFront->x][snekFront->y].in = snekDirection;
+			world[snekFront->next->x][snekFront->next->y].out = snekDirection;
 
-			if (world[front->x][front->y].type == FOOD) {
-				nexttail->next = new Snek;
-				nexttail->next->x = prevtailx;
-				nexttail->next->y = prevtaily;
-				nexttail->next->next = nullptr;
-				world[nexttail->next->x][nexttail->next->y].type = SNEK;
-				world[nexttail->x][nexttail->y].in = prevdir;
-				world[nexttail->next->x][nexttail->next->y].out = prevdir;
+			if (world[snekFront->x][snekFront->y].type == FOOD) {
+				nextTail->next = new Snek;
+				nextTail->next->x = previousTailX;
+				nextTail->next->y = previousTailY;
+				nextTail->next->next = nullptr;
+				world[nextTail->next->x][nextTail->next->y].type = SNEK;
+				world[nextTail->x][nextTail->y].in = previousDirection;
+				world[nextTail->next->x][nextTail->next->y].out = previousDirection;
 
 
-				int nx, ny;
+				int newFoodX, newFoodY;
 				do {
-					nx = rand() % WIDTH;
-					ny = rand() % HEIGHT;
-				} while (world[nx][ny].type != EMPTY);
-				world[nx][ny].type = FOOD;
+					newFoodX = rand() % WIDTH;
+					newFoodY = rand() % HEIGHT;
+				} while (world[newFoodX][newFoodY].type != EMPTY);
+				world[newFoodX][newFoodY].type = FOOD;
 			}
 
-			running = world[front->x][front->y].type != SNEK;
-			world[front->x][front->y].type = SNEK;
+			running = world[snekFront->x][snekFront->y].type != SNEK;
+			world[snekFront->x][snekFront->y].type = SNEK;
 		}
 
 
 		for (int i = 0; i < WIDTH; i++) {
 			for (int j = 0; j < HEIGHT; j++) {
-				a.x = i * FIELD;
-				a.y = j * FIELD;
+				renderTarget.x = i * FIELD;
+				renderTarget.y = j * FIELD;
 				switch (world[i][j].type) {
 				case EMPTY:
-					SDL_RenderCopy(renderer, textures[BACK], nullptr, &a);
+					SDL_RenderCopy(renderer, textures[BACK], nullptr, &renderTarget);
 					break;
 				case SNEK:
 					if (world[i][j].out == NONE)
 						switch (world[i][j].in) {
 						case RIGHT:
-							SDL_RenderCopy(renderer, textures[HEAD], nullptr, &a);
+							SDL_RenderCopy(renderer, textures[HEAD], nullptr, &renderTarget);
 							break;
 						case LEFT:
-							SDL_RenderCopyEx(renderer, textures[HEAD], nullptr, &a, 0, nullptr, SDL_FLIP_HORIZONTAL);
+							SDL_RenderCopyEx(renderer, textures[HEAD], nullptr, &renderTarget, 0, nullptr, SDL_FLIP_HORIZONTAL);
 							break;
 						case UP:
-							SDL_RenderCopyEx(renderer, textures[HEAD], nullptr, &a, 270, nullptr, SDL_FLIP_NONE);
+							SDL_RenderCopyEx(renderer, textures[HEAD], nullptr, &renderTarget, 270, nullptr, SDL_FLIP_NONE);
 							break;
 						case DOWN:
-							SDL_RenderCopyEx(renderer, textures[HEAD], nullptr, &a, 90, nullptr, SDL_FLIP_NONE);
+							SDL_RenderCopyEx(renderer, textures[HEAD], nullptr, &renderTarget, 90, nullptr, SDL_FLIP_NONE);
 							break;
 						}
 
 					if ((world[i][j].out == UP && world[i][j].in == UP) || (world[i][j].out == DOWN && world[i][j].in == DOWN)) {
-						SDL_RenderCopyEx(renderer, textures[BODY], nullptr, &a, 90, nullptr, SDL_FLIP_NONE);
+						SDL_RenderCopyEx(renderer, textures[BODY], nullptr, &renderTarget, 90, nullptr, SDL_FLIP_NONE);
 					}
 					if ((world[i][j].out == LEFT && world[i][j].in == LEFT) || (world[i][j].out == RIGHT && world[i][j].out == RIGHT))
-						SDL_RenderCopy(renderer, textures[BODY], nullptr, &a);
+						SDL_RenderCopy(renderer, textures[BODY], nullptr, &renderTarget);
 
 
 
 					if ((world[i][j].in == UP && world[i][j].out == RIGHT) || (world[i][j].in == LEFT && world[i][j].out == DOWN))
-						SDL_RenderCopy(renderer, textures[TURN], nullptr, &a);
+						SDL_RenderCopy(renderer, textures[TURN], nullptr, &renderTarget);
 					if ((world[i][j].in == DOWN && world[i][j].out == RIGHT) || (world[i][j].in == LEFT && world[i][j].out == UP))
-						SDL_RenderCopyEx(renderer, textures[TURN], nullptr, &a, 0, nullptr, SDL_FLIP_VERTICAL);
+						SDL_RenderCopyEx(renderer, textures[TURN], nullptr, &renderTarget, 0, nullptr, SDL_FLIP_VERTICAL);
 					if ((world[i][j].in == UP && world[i][j].out == LEFT) || (world[i][j].in == RIGHT && world[i][j].out == DOWN))
-						SDL_RenderCopyEx(renderer, textures[TURN], nullptr, &a, 0, nullptr, SDL_FLIP_HORIZONTAL);
+						SDL_RenderCopyEx(renderer, textures[TURN], nullptr, &renderTarget, 0, nullptr, SDL_FLIP_HORIZONTAL);
 					if ((world[i][j].in == RIGHT && world[i][j].out == UP) || (world[i][j].in == DOWN && world[i][j].out == LEFT)) {
-						SDL_RenderCopyEx(renderer, textures[TURN], nullptr, &a, 180, nullptr, SDL_FLIP_NONE);
+						SDL_RenderCopyEx(renderer, textures[TURN], nullptr, &renderTarget, 180, nullptr, SDL_FLIP_NONE);
 					}
 
 
 					if (world[i][j].in == NONE)
 						switch (world[i][j].out) {
 						case RIGHT:
-							SDL_RenderCopy(renderer, textures[TAIL], nullptr, &a);
+							SDL_RenderCopy(renderer, textures[TAIL], nullptr, &renderTarget);
 							break;
 						case LEFT:
-							SDL_RenderCopyEx(renderer, textures[TAIL], nullptr, &a, 0, nullptr, SDL_FLIP_HORIZONTAL);
+							SDL_RenderCopyEx(renderer, textures[TAIL], nullptr, &renderTarget, 0, nullptr, SDL_FLIP_HORIZONTAL);
 							break;
 						case UP:
-							SDL_RenderCopyEx(renderer, textures[TAIL], nullptr, &a, 270, nullptr, SDL_FLIP_NONE);
+							SDL_RenderCopyEx(renderer, textures[TAIL], nullptr, &renderTarget, 270, nullptr, SDL_FLIP_NONE);
 							break;
 						case DOWN:
-							SDL_RenderCopyEx(renderer, textures[TAIL], nullptr, &a, 90, nullptr, SDL_FLIP_NONE);
+							SDL_RenderCopyEx(renderer, textures[TAIL], nullptr, &renderTarget, 90, nullptr, SDL_FLIP_NONE);
 							break;
 						}
 
 					break;
 				case FOOD:
-					SDL_RenderCopy(renderer, textures[FOOD_TEXTURE], nullptr, &a);
+					SDL_RenderCopy(renderer, textures[FOOD_TEXTURE], nullptr, &renderTarget);
 					break;
 				}
 			}
